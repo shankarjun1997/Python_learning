fca58f5ed2595f11909a8cfec89e165effa85731
shankarjun1997 <109965853+shankarjun1997@users.noreply.github.com>
2022-07-25 19:29:07 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,10 @@
+first = 'Shankar'
+last = 'Arjunan'
+msg = f'{first}[{last }] is a coder'
+print(msg.lower())
+print(msg.upper())
+print(msg.find('a'))
+print(msg.replace('a', 'A'))
+print(msg.count('a'))
+print('Shankar' in msg)
+print(len(msg))
